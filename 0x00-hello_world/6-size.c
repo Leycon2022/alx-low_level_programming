15378e5024a31bf43f9f6f8eb6e2b9bb4dc18298
leycon2022 <jephthahaluko@gmailcom>
2022-12-10 23:57:15 -0800

    task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,4 +16,4 @@ printf("size of a long int: %lu byte(s)\n", sizeof(long int));
 printf("size of a long long int: %lu byte(s)\n", sizeof(long long int));
 printf("size of a float: %lu byte(s)\n", sizeof(float));
 return (0);
-} 
+}
